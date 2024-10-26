b4adfd13a5c85926b3b2817b13f7210863e34289
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2024-10-26 17:54:19 +0100

    v3

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -167,7 +167,7 @@ const Header = () => {
         ))}
       </ul>
       <span className="flex items-center gap-5">
-        <span className="fixed top-5 z-[999999999999999999999999] right-7">
+        <span className="fixed top-5 z-[9999999999999999999999999999999999999999999] right-7">
           <span className="absolute z-[9999999999999999999999] -top-[35%] -right-[35%] bg-red-500 text-white p-1 rounded-full w-5 h-5 flex justify-center items-center">
             {order.length}
           </span>
@@ -180,7 +180,7 @@ const Header = () => {
             className="text-[1.8rem] cursor-pointer z-[999999999999999999999999999]"
           />
         </span>
-        <div className="md:hidden" onClick={toggleMenu}>
+        <div className="md:hidden mr-2" onClick={toggleMenu}>
           <GiHamburgerMenu className="text-[1.8rem] cursor-pointer" />
         </div>
       </span>
